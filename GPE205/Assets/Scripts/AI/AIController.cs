77b28dd02b4292200b57fa836c4362b839f0a5ee
Will McClay <wilmccla@uat.edu>
2019-06-05 14:17:57 -0700

    AI Controller Stuff

--- GPE205/Assets/Scripts/AI/AIController.cs
+++ GPE205/Assets/Scripts/AI/AIController.cs
@@ -5,4 +5,27 @@ using UnityEngine;
 public class AIController : MonoBehaviour
 {
     public ShipData data;
+    public float stateStartTime;
+    public float feelerDistance;
+
+    public enum AIStates
+    {
+        Idle, Patrol, TurnToAvoid, MoveToAvoid, Chase, Flee
+    }
+
+    public void ChangeState( AIStates newState)
+    {
+        stateStartTime = Time.time;
+        currentState = newState;
+    }
+
+    public bool isBlocked()
+    {
+        if (Physics.Raycast(data.tf.position, data.tf.forward, feelerDistance))
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
---
+++ GPE205/Assets/Scripts/AI/PatrolAIController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolAIController : AIController
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Switch states based on current state
+        switch (currentState)
+        {
+            case AIStates.Idle:
+                //Run Idle
+                Idle();
+                //Check time for switching states
+                if (Time.time > stateStartTime + 3.0f)
+                {
+                    ChangeState(AIStates.Patrol);
+                }
+                break;
+
+            case AIStates.Patrol:
+                //Run Patrol
+                Patrol();
+                //Check for transition
+                //TODO: Add Transitions
+        }
+    }
+}
