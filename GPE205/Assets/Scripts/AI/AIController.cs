569faf11554250dc097407c9d5861f3f27587b1b
Will McClay <wilmccla@uat.edu>
2019-06-10 14:22:25 -0700

    FSM

--- GPE205/Assets/Scripts/AI/AIController.cs
+++ GPE205/Assets/Scripts/AI/AIController.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class AIController : MonoBehaviour
 {
     public ShipData data;
+    public Transform[] waypoints;
+    public int waypointIndex;
     public float stateStartTime;
     public float feelerDistance;
+    public AIStates currentState;
+
 
     public enum AIStates
     {
@@ -17,6 +21,41 @@ public class AIController : MonoBehaviour
     {
         stateStartTime = Time.time;
         currentState = newState;
+    }
+
+    public void Idle()
+    {
+        //Not much to do here
+    }
+
+    public void Seek(Transform seekTarget)
+    {
+        Vector3 targetVector = (seekTarget.position - data.tf.position).normalized;
+        data.mover.RotateTowards(targetVector);
+        data.mover.SimpleMove(Vector3.forward);
+    }
+
+    public void Flee(Transform fleeTarget)
+    {
+        Vector3 targetVector = (fleeTarget.position - data.tf.position);
+        Vector3 awayVector = -targetVector;
+        data.mover.RotateTowards(awayVector);
+        data.mover.SimpleMove(Vector3.forward);
+    }
+
+    public void Patrol()
+    {
+        Seek(waypoints[waypointIndex]);
+
+        if (Vector3.Distance(data.tf.position, waypoints[waypointIndex].position) <= 0.5f)
+        {
+            waypointIndex++;
+        }
+
+        if (waypointIndex >= waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
     }
 
     public bool isBlocked()
