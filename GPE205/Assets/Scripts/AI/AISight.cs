56a26de25b39c762e4482f23050122612a26321b
William McClay <wilmccla@uat.edu>
2019-06-17 17:30:56 -0700

    AI Stuff

---
+++ GPE205/Assets/Scripts/AI/AISight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script will enable the AI to see the player
+
+public class AISight : MonoBehaviour
+{
+    [Header("Components")]
+    public ShipData data;
+
+    [Header("Variables")]
+    public float radius;
+    [Range(0,360)]
+    public float angle;
+    public LayerMask objectMask;
+    public LayerMask ObstacleMask;
+    public List<Transform> visibleObjects = new List<Transform>();
+
+    void Awake()
+    {
+        data = GetComponent<ShipData>();
+        StartCoroutine("FindObjectsDelay", .2f);
+    }
+
+    IEnumerator FindObjectsDelay(float delay)
+    {
+
+    }
+}
