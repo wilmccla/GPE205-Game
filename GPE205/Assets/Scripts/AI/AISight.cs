2fe5d1e4600c2e1d3551d967f92b2968f5fb3c6f
Will McClay <wilmccla@uat.edu>
2019-06-19 14:23:48 -0700

    Powerup Framework, AI Sight

--- GPE205/Assets/Scripts/AI/AISight.cs
+++ GPE205/Assets/Scripts/AI/AISight.cs
@@ -8,6 +8,7 @@ public class AISight : MonoBehaviour
 {
     [Header("Components")]
     public ShipData data;
+    public Transform tf;
 
     [Header("Variables")]
     public float radius;
@@ -20,11 +21,35 @@ public class AISight : MonoBehaviour
     void Awake()
     {
         data = GetComponent<ShipData>();
+        tf = GetComponent<Transform>();
         StartCoroutine("FindObjectsDelay", .2f);
     }
 
     IEnumerator FindObjectsDelay(float delay)
     {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleObjects();
+        }
+    }
 
+    void FindVisibleObjects()
+    {
+        visibleObjects.Clear();
+        Collider[] objectsInViewRadius = Physics.OverlapSphere(transform.position, radius, objectMask);
+        for (int i = 0; i < objectsInViewRadius.Length; i++)
+        {
+            Transform objects = objectsInViewRadius[i].transform;
+            Vector3 dirToObjects = (objects.position - tf.position).normalized;
+            if (Vector3.Angle(tf.forward, dirToObjects) < angle / 2)
+            {
+                float dstToObjects = Vector3.Distance(tf.position, objects.position);
+                if (!Physics.Raycast(tf.position, dirToObjects, dstToObjects, ObstacleMask))
+                {
+                    visibleObjects.Add(objects);
+                }
+            }
+        }
     }
 }
