3f1cecbdf9fa295093e2e4a10e13c6001648743d
William McClay <wilmccla@uat.edu>
2019-07-29 20:02:58 -0700

    Started on procedural generation

---
+++ GPE205/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public List<GameObject> Rooms;
+    public GameObject[,] Grid;
+
+    public int columns;
+    public int rows;
+
+    private const int roomSize = 60;
+
+
+    void GenerateLevel()
+    {
+        Grid = new GameObject[columns,rows];
+
+        for (int col = 0; col < columns; col++)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                Grid[col, row] = Instantiate(Rooms[Random.Range(0, Rooms.Count)]);
+                Grid[col, row].transform.position = new Vector3(col * roomSize, -row * roomSize);
+            }
+        }
+    }
+}
